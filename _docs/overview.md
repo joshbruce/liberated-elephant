98ad422ecdd8450428d185825a172274cee69331
Josh Bruce <josh@8foldproductivity.com>
2016-12-18 15:52:11 -0500

    Testing GitHub Pages build

--- _docs/overview.md
+++ _docs/overview.md
@@ -1,6 +1,7 @@
 ---
 permalink: /
 layout: default
+title: Liberated Elephant
 ---
 
-<h1 class="test-class">Hello</h1>
+Jessica Katz is a trainer, mentor and coach helping people find and nurture their authentic selves. Through Liberated Elephant, Jessica works with individuals and teams to help them dig deep into self-discovery, and with their new knowledge, helps them find the solutions hidden within their subconscious. Her methodology is informed by Agile project management, creating a safe space for her clients to bring their full selves without judgement, and these five core principles. <link to overview or another page here>
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', [
+  'sass'
+]);
+
+gulp.task('release', ['sass'], function() {
+  var entryFile = '_presentation-assets/stylesheets/le-main.scss';
+  gulp.src(entryFile)
+    // .pipe(sourcemaps.init())
+    .pipe(
+        sass({ outputStyle: 'expanded' })
+            .on('error', sass.logError)
+    )
+    // .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('assets/presentation/css/'));
+});
+
+// Sass
+gulp.task('sass', [
+  'copy-normalize',
+  'copy-bourbon',
+  'copy-bourbon-neat'
+]);
+
+gulp.task('copy-normalize', function() {
+  gulp.src('node_modules/normalize.css/normalize.css')
+      .pipe(gulp.dest('_presentation-assets/stylesheets/vendor/normalize.css/'));
+});
+
+gulp.task('copy-bourbon', function() {
+  gulp.src('node_modules/bourbon/app/assets/stylesheets/**/*.scss')
+      .pipe(gulp.dest('_presentation-assets/stylesheets/vendor/bourbon/'));
+});
+
+gulp.task('copy-bourbon-neat', function() {
+  gulp.src('node_modules/bourbon-neat/app/assets/stylesheets/**/*.scss')
+      .pipe(gulp.dest('_presentation-assets/stylesheets/vendor/bourbon-neat/'));
+});
